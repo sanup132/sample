76fd2a93cd79b08d666599b3362036c5f05e41b6
Sanup Kumar <70508203+sanup132@users.noreply.github.com>
2023-05-15 09:40:29 +0530

    Update division.java
    You have just write Bhopal

--- division.java
+++ division.java
@@ -1,5 +1,8 @@
 public class division {
     public static void main(String[] args) {
-        System.out.println("Bhopal");
+       // System.out.println("Bhopal");
+        int a =56;
+        int b =12;
+        System.out.print(a/b);
     }
 }
